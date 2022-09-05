29871e360b6da3464cdb9a6bede9e6977933e6d9
Ali Mora <ali@openmindi.co.za>
2022-09-05 10:39:16 +0000

    pages upadte

---
+++ pages/dashboard.vue
@@ -0,0 +1,22 @@
+<template>
+   <div class="h-screen bg-gradient-to-b from-white to-gray-20">
+    <div class="flex items-center justify-center  px-4 sm:px-6 lg:px-8">
+      <div class="max-w-md w-full space-y-8">
+        <div>
+          <div class="h-25 w-25">
+          </div>
+          <div class="flex">
+          <img class="mx-auto h-24 w-auto" src="/img/mtn-logo-footer.svg" alt="full stack jack logo" />
+          <h1 class="py-9 text-center text-5xl font-extrabold text-gray-900 dark:text-gray-800">Teacha</h1>
+          </div>
+          
+          <h2 class="mt-6 py-9 text-center text-3xl font-extrabold text-gray-900 dark:text-gray-800">Dashboard page</h2>
+        </div>
+
+  
+
+       
+      </div>
+    </div>
+  </div>
+</template>
